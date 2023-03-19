599f44c9683aafcd8f8a89b63cf18f2167fa1f50
NguyenVietHoangg <nvhoangtb2k2@gmail.com>
2023-03-19 10:00:40 +0700

    cấu hình Prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     <h1>hello anh em</h1>
-     
-    </div>
-  );
+      return (
+            <div className="App">
+                  <h1>hello anh em</h1>
+            </div>
+      );
 }
 
 export default App;
